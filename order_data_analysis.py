342c25a0ba4f5e1702652c6be7bdb716f15806e9
Shivas <shivas.2004003@gmail.com>
2026-01-16 15:39:32 +0530

    Update order_data_analysis.py

--- order_data_analysis.py
+++ order_data_analysis.py
@@ -1,49 +1,43 @@
-import libraries
-
 !pip install kaggle
-import kaggle
 
+import kaggle
 !kaggle datasets download ankitbansal06/retail-orders -f orders.csv
 
+import zipfile
+zip_ref = zipfile.ZipFile('orders.csv.zip')
+zip_ref.extractall()
+zip_ref.close()
 
 #read data from the file and handle null values
 import pandas as pd
-df = pd.read_csv('orders.csv',na_values=['Not Available','unknown'])
-df['Ship Mode'].unique()
+orders_data = pd.read_csv('orders.csv',na_values=['Not Available','unknown'])
+orders_data['Ship Mode'].unique()
 
 orders_data.isnull()
 
-
-#rename column names ..make them lower case and replace spaces withunderscorese
+#orders_data.rename(columns={'Order Id':'order_id','City':'city',}) #this is not a good practics
+#orders_data.head(3)
 
 orders_data.columns=orders_data.columns.str.lower().str.replace(' ','_')
 orders_data.head()
 
+orders_data.dtypes
+
+orders_data['order_date']= pd.to_datetime(orders_data['order_date'])
+
 orders_data.shape
 
-
 #derive new columns: discount, sale price and profit
-df['discount']=df['list_price']*df['discount_percent']/100
+orders_data['discount']=orders_data['list_price']*orders_data['discount_percent']/100
+orders_data
 
-df['sale_price']= df['list_price']-df['discount']
+orders_data['sale_price']= orders_data['list_price']-orders_data['discount']
 
-df['profit']=df['sale_price']-df['cost_price']
-
-df
-
-
-df.dtypes
+orders_data['profit']=orders_data['sale_price']-orders_data['cost_price']
+orders_data
 
 #convert order date from object data type to datetime
-df['order_date']=pd.to_datetime(df['order_date'],format="%Y-%m-%d")
+orders_data['order_date']=pd.to_datetime(orders_data['order_date'],format="%Y-%m-%d")
 
-
-
-
-
-#drop cost, price list price and discount percent columns
-df.drop(columns=['list_price','cost_price','discount_percent'],inplace=True)
-
-
-
-
+#drop cost, price, list price, and discount percent columns
+orders_data.drop(columns=['list_price','cost_price','discount_percent'],inplace=True)
